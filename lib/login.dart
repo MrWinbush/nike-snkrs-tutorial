3a626b889e218fd467a071ddc42ddb65a3dca34d
Morgan Winbush <m.winbush@ynotlabsllc.com>
2020-09-03 22:32:09 -0400

    Completed Login screen with PageView

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nike_flutter_app/login_button.dart';
+import 'package:nike_flutter_app/pre_login_marketing_screen.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key, this.title}) : super(key: key);
@@ -10,29 +12,65 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final controller = PageController(initialPage: 0);
+  List<PreLoginInfo> preLoginInfo = [
+      PreLoginInfo("", "Welcome to Nike SNKRS. Your ultimate sneaker destination", "snkrs.png"),
+      PreLoginInfo("Purchase Quickly", "Buy sneakers in seconds, direclty within the app. Store all your info to expedite the process.", "airJordan1s.jpg"),
+      PreLoginInfo("Stay a Step Ahead", "Set notifications for upcoming releases. Share news, photos and videos with friends.", "jordanChileRed.jpg"),
+      PreLoginInfo("Get The Story", "learn about the inspiration, benefits and heritage of featured sneakers straight from the source.", "kobe5Pronto.jpg")
+    ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              LoginButton("Log In", Colors.white, Colors.black, borderColor: Colors.grey[300],),
-              SizedBox(width: 10,),
-              LoginButton("Join Now", Colors.black, Colors.white)
-            ],
-          )
+          padding: EdgeInsets.fromLTRB(0, 50, 0, 30),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                child: PageView(
+                  controller: controller,
+                  scrollDirection: Axis.horizontal, 
+                  children: this.preLoginInfo.map((loginMarketing) => PreLoginMarketingScreen(loginMarketing)).toList()
+                  )
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 15, 0, 20),
+                  child: SmoothPageIndicator(
+                    controller: controller,
+                    count: preLoginInfo.length,
+                    effect: SlideEffect(
+                      dotWidth: 6,
+                      dotHeight: 6,
+                      activeDotColor: Colors.black
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    LoginButton("Log In", Colors.white, Colors.black, borderColor: Colors.grey[300],),
+                    SizedBox(width: 10,),
+                    LoginButton("Join Now", Colors.black, Colors.white)
+                  ],
+                ),
+                FlatButton(onPressed: null, child: Text("Continue as Guest")),
+                SizedBox(height: 40,),
+              ],
+            ),
         ),
       )
     );
   }
+}
+
+class PreLoginInfo {
+  String title;
+  String description;
+  String imagePath;
+
+  PreLoginInfo(this.title, this.description, this.imagePath);
 }
---
+++ lib/pre_login_marketing_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class PreLoginMarketingScreen extends StatelessWidget {
+  PreLoginInfo preLoginInfo;
+
+  PreLoginMarketingScreen(this.preLoginInfo);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/${this.preLoginInfo.imagePath}', height: 300,),
+            Text(this.preLoginInfo.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            SizedBox(height: 15,),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 0, 80, 0),
+              child: Text(this.preLoginInfo.description, textAlign: TextAlign.center),
+            )
+          ],
+        ),
+    );
+  }
+}
